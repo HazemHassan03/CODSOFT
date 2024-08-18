76f2e14d1d6224ae2242974cc9be0334fb63f51b
Hazem Hassan Mahmoud <hazemhassan0321@gmail.com>
2024-08-18 20:35:52 +0300

    Update main.js

--- Digital-Clock/JS/main.js
+++ Digital-Clock/JS/main.js
@@ -1,6 +1,7 @@
 let hour = document.getElementById("hour");
 let minute = document.getElementById("min");
 let second = document.getElementById("sec");
+let amOrPm = document.getElementById("am-or-pm");
 function update() {
   let date = new Date();
   if (date.getHours() < 10) {
@@ -17,6 +18,17 @@ function update() {
     second.textContent = `0${date.getSeconds()}`;
   } else {
     second.textContent = date.getSeconds();
+  }
+  if (hour.textContent > 12) {
+    hour.textContent -= 12;
+    if (hour.textContent < 10) {
+      hour.textContent = `0${hour.textContent}`;
+    }
+  }
+  if (date.getHours() < 12) {
+    amOrPm.textContent = "AM";
+  } else {
+    amOrPm.textContent = "PM";
   }
 }
 update();
